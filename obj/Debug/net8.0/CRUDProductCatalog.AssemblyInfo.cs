42a872beac958b4eb0c1ae14bc266525fa9360e1
Carlillos-M <al44552@un.edu.mx>
2024-06-14 00:16:22 -0600

    Programa funcionando final 2

--- obj/Debug/net8.0/CRUDProductCatalog.AssemblyInfo.cs
+++ obj/Debug/net8.0/CRUDProductCatalog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDProductCatalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+741222874670f9155f47191fd9397615f94d63c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4d6f0e11f018700a513592e88145be30cd3850")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDProductCatalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDProductCatalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
